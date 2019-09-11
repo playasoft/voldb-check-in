4f632b16ff561f92f932d1ee62cf9dade86b039e
Luminesce0 <dominuskelenth@gmail.com>
2019-09-10 20:10:18 -0600

    Feature: Role list RecyclerView Fragment for database UI
    After selecting a role a list of shifts will populate.

---
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/shifts/ShiftListViewModel.kt
@@ -0,0 +1,34 @@
+package net.wetfish.playasoftvolunteers.ui.shifts
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import net.wetfish.playasoftvolunteers.PlayasoftVolunteers
+import net.wetfish.playasoftvolunteers.data.model.Shift
+
+/**
+ * Created by ${Michael} on 8/17/2019.
+ */
+class ShiftListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val userInfoRepository = getApplication<PlayasoftVolunteers>().getUserInfoRepository()
+    private val shiftList = MediatorLiveData<List<Shift>>()
+
+    init {
+        getAllShifts()
+    }
+
+    // 1
+    fun getShiftList(): LiveData<List<Shift>> {
+        return shiftList
+    }
+
+    // 2
+    fun getAllShifts() {
+        shiftList.addSource(userInfoRepository.getShifts()) { roles ->
+            shiftList.postValue(roles)
+        }
+    }
+
+}
