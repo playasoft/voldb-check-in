50e520fba0e73e3d68c79565f704a3f866be2cc7
Luminesce0 <dominuskelenth@gmail.com>
2019-10-10 05:09:48 -0600

    Feature: Appropriately setup data models for JSON responses
    Added proper serialized names to retain proper naming conventions within Kotlin.

--- app/src/main/java/net/wetfish/playasoftvolunteers/data/model/Role.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/data/model/Role.kt
@@ -2,14 +2,24 @@ package net.wetfish.playasoftvolunteers.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 /**
  * Created by ${Michael} on 8/12/2019.
  */
 @Entity
 data class Role(
-    var role_id: String,
-    var department_id: String,
-    var name: String,
+    @SerializedName("role_id")
+    @Expose
+    var roleId: String,
+
+    @SerializedName("department_id")
+    @Expose
+    var departmentId: String,
+
+    @SerializedName("name")
+    @Expose
+    var roleName: String,
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 )
