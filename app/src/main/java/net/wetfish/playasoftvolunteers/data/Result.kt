37207ba855d55087700ae887c87f13851f14ea61
Luminesce0 <dominuskelenth@gmail.com>
2019-11-06 17:28:54 -0700

    Temp commit

--- app/src/main/java/net/wetfish/playasoftvolunteers/data/Result.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/data/Result.kt
@@ -4,15 +4,25 @@ package net.wetfish.playasoftvolunteers.data
  * A generic class that holds a value with its loading status.
  * @param <T>
  */
-sealed class Result<out T : Any> {
+data class Result<out T>(val status: Status, val data: T?, val message: String?) {
 
-    data class Success<out T : Any>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
+    enum class Status {
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
 
-    override fun toString(): String {
-        return when (this) {
-            is Success<*> -> "Success[data=$data]"
-            is Error -> "Error[exception=$exception]"
+    companion object {
+        fun <T> success(data: T): Result<T> {
+            return Result(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(message: String, data: T? = null): Result<T> {
+            return Result(Status.ERROR, data, message)
+        }
+
+        fun <T> loading(data: T? = null): Result<T> {
+            return Result(Status.LOADING, data, null)
         }
     }
 }
--- app/src/main/java/net/wetfish/playasoftvolunteers/ui/departments/DepartmentListAdapter.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/departments/DepartmentListAdapter.kt
@@ -3,6 +3,9 @@ package net.wetfish.playasoftvolunteers.ui.departments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.layout_list_department_item.view.*
 import net.wetfish.playasoftvolunteers.R
@@ -11,10 +14,26 @@ import net.wetfish.playasoftvolunteers.data.model.Department
 /**
  * Created by ${Michael} on 8/16/2019.
  */
-class DepartmentListAdapter(
-    private val items: List<Department>,
-    private val clickListener: OnItemClickListener
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DepartmentListAdapter : PagedListAdapter<Department, DepartmentListAdapter.ViewHolder>(DepartmentListDiffCallback()) {
+
+    private lateinit var recyclerView: RecyclerView
+
+    /**
+     * Binds view with item info
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val department = getItem(position)
+        department?.let {
+            holder.apply {
+                bind(createOnClickListener(department.departmentId), department)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ListItemDepartmentBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false))
+    }
 
     /**
      * Notifies click on an item with attached view
@@ -24,46 +43,58 @@ class DepartmentListAdapter(
     }
 
     /**
-     * Creates view for each item in the list
+     * Creates onClickListener for each item in the list
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_list_department_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    /**
-     * Binds view with item info
-     */
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(items[position], clickListener)
-    }
-
-    /**
-     * Returns the size to item list
-     */
-    override fun getItemCount(): Int {
-        return items.size
+    private fun createOnClickListener(id: String): View.OnClickListener {
+        return View.OnClickListener {
+            val direction = DepartmentListFragmentDirections.actionToRolesListFragment(id)
+            it.findNavController().navigate(direction)
+        }
     }
 
     /**
      * View for item, sets item info and click departments
      */
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val binding: list_item_department)
+        : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(department: Department, listener: OnItemClickListener) = with(itemView) {
-            tv_departmentName.text = department.name
-            setOnClickListener {
-
-                //TODO:: Figure this out later
-            }
-
-            // RecyclerView on item click
-            setOnClickListener {
-                listener.onItemClick(department, it)
+        fun bind(listener: View.OnClickListener, item: Department) {
+            binding.apply {
+                clickListener = listener
+                department = item
+                executePendingBindings()
             }
         }
 
     }
 
+//    class ViewHolder(private val binding: ListItemDepartmentBinding)
+//        : RecyclerView.ViewHolder(bi) {
+//
+//        fun bind(department: Department, listener: OnItemClickListener) = with(itemView) {
+//            tv_departmentName.text = department.departmentName
+//            setOnClickListener {
+//
+//                //TODO:: Figure this out later
+//            }
+//
+//            // RecyclerView on item click
+//            setOnClickListener {
+//                listener.onItemClick(department, it)
+//            }
+//        }
+//
+//    }
+
+}
+
+private class DepartmentListDiffCallback : DiffUtil.ItemCallback<Department>() {
+
+    override fun areItemsTheSame(oldItem: Department, newItem: Department): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Department, newItem: Department): Boolean {
+        return oldItem == newItem
+    }
 }
