7d80d7b1bf4db5fe635939145b039f1cf3c9a25b
Luminesce0 <dominuskelenth@gmail.com>
2019-12-17 19:44:23 -0700

    Feature: Clean up Adapter
    Appropriately put ViewModel data into the ViewModel class.

--- app/src/main/java/net/wetfish/playasoftvolunteers/ui/events/EventListAdapter.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/events/EventListAdapter.kt
@@ -12,7 +12,7 @@ import net.wetfish.playasoftvolunteers.data.model.Event
  * Created by ${Michael} on 8/16/2019.
  */
 class EventListAdapter(
-    private val items: List<Event>,
+    private val eventData: List<Event>,
     private val clickListener: OnItemClickListener
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -27,42 +27,44 @@ class EventListAdapter(
      * Creates view for each item in the list
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_list_event_item, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
     /**
      * Binds view with item info
      */
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(items[position], clickListener)
+        (holder as ViewHolder).bind(eventData[position], clickListener)
     }
 
     /**
      * Returns the size to item list
      */
     override fun getItemCount(): Int {
-        return items.size
+        return eventData.size
     }
 
     /**
      * View for item, sets item info and click events
      */
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(event: Event, listener: OnItemClickListener) = with(itemView) {
             tv_eventName.text = event.eventName
             tv_eventStart.text = event.startDate
             tv_eventEnd.text = event.endDate
-            setOnClickListener {
-
-                //TODO:: Figure this out later
-            }
 
             // RecyclerView on item click
             setOnClickListener {
                 listener.onItemClick(event, it)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.layout_list_event_item, parent, false)
+                return ViewHolder(view)
             }
         }
 
