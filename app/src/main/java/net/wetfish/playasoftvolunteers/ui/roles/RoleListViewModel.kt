5a004c364c2a8751242ca778872c68e23d2b7c9b
Luminesce0 <dominuskelenth@gmail.com>
2019-12-24 04:20:38 -0700

    Refactor: RoleListViewModel
    Base refactor for the RoleListViewModel

--- app/src/main/java/net/wetfish/playasoftvolunteers/ui/roles/RoleListViewModel.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/roles/RoleListViewModel.kt
@@ -3,38 +3,53 @@ package net.wetfish.playasoftvolunteers.ui.roles
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import net.wetfish.playasoftvolunteers.App
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import net.wetfish.playasoftvolunteers.data.db.UserDao
 import net.wetfish.playasoftvolunteers.data.model.Role
 
 /**
  * Created by ${Michael} on 8/17/2019.
  */
-class RoleListViewModel(dataSource: UserDao, role: Int, application: Application) : AndroidViewModel(application) {
+class RoleListViewModel(
+    database: UserDao,
+    roleKey: Long,
+    application: Application
+) : AndroidViewModel(application) {
 
-    private val userInfoRepository = getApplication<App>().getUserInfoRepository()
-    private val roleList = MediatorLiveData<List<Role>>()
+    private var viewModelJob = Job()
 
-    //TODO: This initialization may be pointless
-//    init {
-//        getAllRoles()
-//    }
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    fun getRoleList(departmentID: Int): LiveData<List<Role>> {
-        findRoles(departmentID)
-        return roleList
+    var rolesList = database.findRoles(roleKey)
+
+    private val roles = MutableLiveData<LiveData<List<Role>>>()
+
+    fun getRoles() = roles
+
+    init {
+        roles.postValue(database.findRoles(roleKey))
     }
 
-//    fun getAllRoles() {
-//        roleList.addSource(userInfoRepository.getRoles()) { roles ->
-//            roleList.postValue(roles)
-//        }
-//    }
+    // Data that will be passed from the fragment
+    private val _navigateToRoleList = MutableLiveData<Long>()
 
-    fun findRoles(eventID: Int) {
-        roleList.addSource(userInfoRepository.findRoles(eventID)) { roles ->
-            roleList.postValue(roles)
-        }
+    // Getter for what the fragment will observe
+    val navigateToRoleList get() = _navigateToRoleList
+
+    fun onDepartmentClicked(id: Long) {
+        _navigateToRoleList.value = id
     }
+
+    fun doneNavigating() {
+        _navigateToRoleList.value = null
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
 }
