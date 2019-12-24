70c18ee0b779abd8dfd16fdc81155d20865b11ae
Luminesce0 <dominuskelenth@gmail.com>
2019-12-24 04:21:42 -0700

    Feature & Refactor: RoleListFragment
    Implement DataBinding into RoleListFragment along with ViewModelFactory
    and Navigation.

--- app/src/main/java/net/wetfish/playasoftvolunteers/ui/roles/RoleListFragment.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/roles/RoleListFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_role_list.*
 import net.wetfish.playasoftvolunteers.R
+import net.wetfish.playasoftvolunteers.data.db.VolunteerDatabase
 import net.wetfish.playasoftvolunteers.data.model.Role
+import net.wetfish.playasoftvolunteers.databinding.FragmentRoleListBinding
+import net.wetfish.playasoftvolunteers.ui.departments.DepartmentListFragment
 
 /**
  * The Fragment to show the role list
@@ -18,56 +23,49 @@ import net.wetfish.playasoftvolunteers.data.model.Role
 class RoleListFragment : Fragment(),
     RoleListAdapter.OnItemClickListener {
 
-    // ViewModel access
-    private lateinit var viewModel: RoleListViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-
-        // ViewModel initialization
-        viewModel = ViewModelProviders.of(this).get(RoleListViewModel::class.java)
-    }
+    // Logging Tag
+    private val TAG = RoleListFragment::class.qualifiedName
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_role_list, container, false)
-    }
+        val binding: FragmentRoleListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_role_list,
+            container,
+            false
+        )
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val args = RoleListFragmentArgs.fromBundle(arguments!!)
 
-        // Gather the roleId to get the appropriate Roles
-        val roleID = arguments?.getInt(getString(R.string.department_id))
+        val application = requireNotNull(this.activity).application
 
-        // Start observing role list
-        viewModel.getRoleList(roleID!!).observe(this, Observer<List<Role>> { roles ->
-            roles?.let {
-                populateRoleList(roles)
+        val dataSource = VolunteerDatabase.getInstance(application).userDao
+
+        val viewModelFactory =
+            RoleListViewModelFactory(dataSource, args.departmentId, application)
+
+        val viewModel = ViewModelProviders.of(
+            this, viewModelFactory).get(RoleListViewModel::class.java)
+
+        binding.roleListViewModel = viewModel
+
+        binding.setLifecycleOwner {this}
+
+        viewModel.navigateToRoleList.observe(this, Observer {roleId ->
+            roleId.let {
+                this.findNavController().navigate(
+                    RoleListFragmentDirections.actionRoleListFragmentToShiftListFragment(
+                        roleId
+                    )
+                )
+
+                viewModel.doneNavigating()
             }
         })
-    }
 
-    /**
-     * Populates peopleRecyclerView with all people info
-     */
-    private fun populateRoleList(roleList: List<Role>) {
-        rv_roles.adapter = RoleListAdapter(roleList, this)
-    }
-
-    /**
-     * Navigates to people details on item click
-     */
-    override fun onItemClick(role: Role, itemView: View) {
-        // Get the role ID and bundle it for transferring to shifts
-        val roleBundle = Bundle().apply {
-            putInt(getString(R.string.role_id), (role.roleId).toInt())
-        }
-
-        view?.findNavController()
-            ?.navigate(R.id.action_roleListFragment_to_shiftListFragment, roleBundle)
+        return binding.root
     }
 
 }
