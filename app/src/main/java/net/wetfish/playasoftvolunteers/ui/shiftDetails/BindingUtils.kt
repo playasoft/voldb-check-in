ed1ce478def2b61cfcffb358a830325a4f8346c3
Luminesce0 <dominuskelenth@gmail.com>
2020-02-03 02:15:37 -0700

    Feature: Navigation Completion & ROOM
    Navigation is now possible among all fragments and appropriately provides
    the desired information from the database.

--- app/src/main/java/net/wetfish/playasoftvolunteers/ui/shiftDetails/BindingUtils.kt
+++ app/src/main/java/net/wetfish/playasoftvolunteers/ui/shiftDetails/BindingUtils.kt
@@ -11,93 +11,84 @@ import net.wetfish.playasoftvolunteers.data.model.Shift
 @BindingAdapter("shift_id")
 fun TextView.setShiftId(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.shiftId
     }
 }
 
 @BindingAdapter("department_id")
 fun TextView.setDepartmentId(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.departmentId
     }
 }
 
 @BindingAdapter("role_id")
 fun TextView.setRoleId(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.roleId
     }
 }
 
 @BindingAdapter("start_date")
 fun TextView.setStartDate(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.startDate
     }
 }
 
 @BindingAdapter("end_date")
 fun TextView.setEndDate(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.endDate
     }
 }
 
 @BindingAdapter("start_time")
 fun TextView.setStartTime(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.startTime
     }
 }
 
 @BindingAdapter("end_time")
 fun TextView.setEndTime(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.endTime
     }
 }
 
 @BindingAdapter("user_id")
 fun TextView.setUserId(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.userId
     }
 }
 
 @BindingAdapter("email")
 fun TextView.setEmail(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.email
     }
 }
 
 @BindingAdapter("full_name")
 fun TextView.setFullName(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.fullName
     }
 }
 
 @BindingAdapter("display_name")
 fun TextView.setDisplayName(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.displayName
     }
 }
 @BindingAdapter("status")
 fun TextView.setStatus(item: Shift?) {
     item?.let{
-        text  = item.eventId
+        text  = item.status
     }
 }
 
 //TODO: Setup spinner later
-//@BindingAdapter("onItemSelected")
-//fun Spinner.setItemSelectedListener(itemSelectedListener: ItemSelectedListener?) {
-//    setSpinnerItemSelectedListener(itemSelectedListener)
-//}
-
-//@BindingAdapter("newValue")
-//fun Spinner.setNewValue(newValue: Any?) {
-//    setSpinnerValue(newValue)
-//}
